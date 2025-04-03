623a1fe2f3012979ca18b68423c8b73914ff818b
kipp2 <kray435@gmail.com>
2025-04-03 14:18:19 +0300

    updated and removed the issue causing the rect points not to show

---
+++ poi.py
@@ -0,0 +1,30 @@
+from rects import *
+
+
+def draw_text(text, x, y, color=BLACK):
+    label = font.render(text, True, color)
+    screen.blit(label, (x, y))
+
+
+pts = ('topleft', 'topright', 'bottomleft', 'bottomright',
+       'midtop', 'midright', 'midbottom', 'midleft', 'center')
+
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == QUIT:
+            running = False         
+
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, GREEN, rect, 4)
+    for pt in pts:
+        pos = eval ('rect.'+pt)
+        draw_text(pt, pos[0], pos[1])
+        pygame.draw.circle(screen, RED, pos, 3)
+
+    update_screen()
+
+pygame.quit()
