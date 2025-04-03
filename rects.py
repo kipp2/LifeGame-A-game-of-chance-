dc2718e4a12eedd4feb20fc9f752e847a4c3f85c
kipp2 <kray435@gmail.com>
2025-04-03 14:22:20 +0300

    The change has now rendered this window open and close

---
+++ rects.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+
+SIZE = 500, 200
+RED =(255, 0, 0)
+GRAY = (150, 150, 150)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+width, height = SIZE 
+
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+font = pygame.font.Font(None, 14)
+
+def update_screen():
+    pygame.display.flip()
+screen.fill(GRAY)
+
+rect = Rect(50, 60, 200, 80)
+print(f'x = {rect.x}, y = {rect.y}, w={rect.w}, h ={rect.h}')
+print(f'lefy={rect.left}, top={rect.top}, right= {rect.right}, bottom={rect.bottom}')
+print(f'center={rect.center} ')
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    pygame.draw.rect(screen, RED, rect)
+    pygame.display.flip()
+    break
